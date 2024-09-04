0c2bb23387d37e612fb811270ac3e0540909e018
RVSmooth <itzsubhashis@proton.me>
2024-09-04 10:54:05 +0530

    config: fish: add function for dotfiles bare repo and ssh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,26 @@ set -g fish_greeting
 set -x TERM xterm-256color
 set PATH $PATH ~/.local/bin
 
-alias config="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+function ssh_git
+	eval (ssh-agent -c)
+	ssh-add $argv
+end
+
+function config
+    set -l git_cmd "/usr/bin/git"
+    set -l git_options "--git-dir=$HOME/.dotfiles --work-tree=$HOME"
+    
+    set -l untracked_files (eval $git_cmd $git_options config --get status.showUntrackedFiles)
+    
+    if test -z "$untracked_files" -o "$untracked_files" = "all"
+        eval $git_cmd $git_options config --unset status.showUntrackedFiles
+        eval $git_cmd $git_options config status.showUntrackedFiles no
+        echo "UntrackedFiles was set to 'all' or not set. Changed to 'no'."
+    else
+        echo "UntrackedFiles is already set to no."
+    end
+    
+    eval $git_cmd $git_options $argv
+end
 
 
