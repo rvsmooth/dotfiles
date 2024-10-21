2f3b383d23f51694544bfb149e72af8715d97269
RVSmooth <itzsubhashis@proton.me>
2024-10-21 18:34:33 +0530

    config: qtile: Indent and remove some widgets

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -25,21 +25,32 @@
 # SOFTWARE.
 import os
 import subprocess
-from libqtile import bar, layout, qtile, hook
-from libqtile.config import Click, Drag, Group, Key, Match, Screen, KeyChord, ScratchPad, DropDown
-from libqtile.lazy import lazy
 from pathlib import Path
+
+import colors
+from libqtile import bar, hook, layout, qtile
+from libqtile.config import (
+    Click,
+    Drag,
+    DropDown,
+    Group,
+    Key,
+    KeyChord,
+    Match,
+    ScratchPad,
+    Screen,
+)
+from libqtile.lazy import lazy
 from qtile_extras import widget
 from qtile_extras.widget.decorations import RectDecoration
-import colors
 
 # Strings
-mod       = "mod4"
-terminal  = "kitty"
-browser   = "brave"
-fileman   = "pcmanfm"
-rofi      = "rofi -show drun"
-Emacs = "emacsclient -c -a 'emacs' " # The space at the end is IMPORTANT!
+mod = "mod4"
+terminal = "kitty"
+browser = "brave"
+fileman = "pcmanfm"
+rofi = "rofi -show drun"
+Emacs = "emacsclient -c -a 'emacs' "  # The space at the end is IMPORTANT!
 home = str(Path.home())
 
 # Keybindings
@@ -47,83 +58,126 @@ keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    Key([mod], "left",  lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "left", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "right", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "down",  lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "up",    lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "down", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "up", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "tab", lazy.layout.next(), desc="Move window focus to other window"),
-
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-   
-      # Grow/shrink windows left/right. 
+    # Grow/shrink windows left/right.
     # This is mainly for the 'monadtall' and 'monadwide' layouts
     # although it does also work in the 'bsp' and 'columns' layouts.
-    Key([mod], "equal",
+    Key(
+        [mod],
+        "equal",
         lazy.layout.grow_left().when(layout=["bsp", "columns"]),
         lazy.layout.grow().when(layout=["monadtall", "monadwide"]),
-        desc="Grow window to the left"
+        desc="Grow window to the left",
     ),
-    Key([mod], "minus",
+    Key(
+        [mod],
+        "minus",
         lazy.layout.grow_right().when(layout=["bsp", "columns"]),
         lazy.layout.shrink().when(layout=["monadtall", "monadwide"]),
-        desc="Grow window to the left"
+        desc="Grow window to the left",
     ),
-
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"],"Return",lazy.layout.toggle_split(),desc="Toggle between split and unsplit sides of stack"),
-    
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
     # Toggle between different layouts as defined below
-    Key([mod], "Tab",          lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "space",            lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
-    Key([mod], "t",            lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "shift"], "c",   lazy.window.kill(), desc="Kill focused window"),
-
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key(
+        [mod],
+        "space",
+        lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen on the focused window",
+    ),
+    Key(
+        [mod],
+        "t",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window",
+    ),
+    Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
     # Basic utils
-    Key([mod, "shift"], "r",   lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r",            lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     # Application Binds
-    Key([mod], "Return",     lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "p",          lazy.spawn(rofi), desc="Launch Rofi run launcher"),
-    Key([mod], "b",          lazy.spawn(browser), desc="Launch browser"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "p", lazy.spawn(rofi), desc="Launch Rofi run launcher"),
+    Key([mod], "b", lazy.spawn(browser), desc="Launch browser"),
     Key([mod, "shift"], "f", lazy.spawn(fileman), desc="Launch file-manager"),
-    Key([mod], "u",          lazy.spawn(home + "/.config/rofi/scripts/rofi-utils"), desc="Launch rofi utilities script"),
-    Key([mod], "e", lazy.spawn(Emacs), desc='Emacs Dashboard'),
-
+    Key(
+        [mod],
+        "u",
+        lazy.spawn(home + "/.config/rofi/scripts/rofi-utils"),
+        desc="Launch rofi utilities script",
+    ),
+    Key([mod], "e", lazy.spawn(Emacs), desc="Emacs Dashboard"),
     # Volume Management
-    Key([mod], "F3",          lazy.spawn(home + "/.config/scripts-common/volume.sh --pw-incvol"), desc="Increase Volume(Pipewire)"),
-    Key([mod], "F1",          lazy.spawn(home + "/.config/scripts-common/volume.sh --pw-decvol"), desc="Decrease Volume(Pipewire)"),
-    Key([mod], "F2",          lazy.spawn(home + "/.config/scripts-common/volume.sh --pw-mute"), desc="Mute Volume(Pipewire)"),
-
+    Key(
+        [mod],
+        "F3",
+        lazy.spawn(home + "/.config/scripts-common/volume.sh --pw-incvol"),
+        desc="Increase Volume(Pipewire)",
+    ),
+    Key(
+        [mod],
+        "F1",
+        lazy.spawn(home + "/.config/scripts-common/volume.sh --pw-decvol"),
+        desc="Decrease Volume(Pipewire)",
+    ),
+    Key(
+        [mod],
+        "F2",
+        lazy.spawn(home + "/.config/scripts-common/volume.sh --pw-mute"),
+        desc="Mute Volume(Pipewire)",
+    ),
     # Screenshots
-    Key([], "Print",          lazy.spawn("flameshot screen"), desc="Take a full screenshot"),
-    Key([mod], "Print",       lazy.spawn("flameshot gui"), desc="Take a partial screenshot"),
-
+    Key([], "Print", lazy.spawn("flameshot screen"), desc="Take a full screenshot"),
+    Key([mod], "Print", lazy.spawn("flameshot gui"), desc="Take a partial screenshot"),
     # variety keybindings
-    KeyChord([mod], "v", [
-        Key([], "n", lazy.spawn("variety --next"), desc='Listen to online radio'),
-        Key([], "p", lazy.spawn("variety --previous"), desc='Search various engines'),
-        Key([], "t", lazy.spawn("variety --trash"), desc='Translate text'),
-        Key([], "f", lazy.spawn("variety --favorite"), desc='Translate text')
-    ]),
-
+    KeyChord(
+        [mod],
+        "v",
+        [
+            Key([], "n", lazy.spawn("variety --next"), desc="Listen to online radio"),
+            Key(
+                [], "p", lazy.spawn("variety --previous"), desc="Search various engines"
+            ),
+            Key([], "t", lazy.spawn("variety --trash"), desc="Translate text"),
+            Key([], "f", lazy.spawn("variety --favorite"), desc="Translate text"),
+        ],
+    ),
 ]
 # Add key bindings to switch VTs in Wayland.
 # We can't check qtile.core.name in default config as it is loaded before qtile is started
@@ -140,12 +194,12 @@ for vt in range(1, 8):
 
 # Workspaces
 groups = [
-    Group("1", layout='monadtall', label='●'),
-    Group("2", layout='monadtall', label='●'),
-    Group("3", layout='monadtall', label='●'),
-    Group("4", layout='tile',      label='●'),
-    Group("5", layout='max',       label='●'),
-    Group("6", layout='floating',  label='●'),
+    Group("1", layout="monadtall", label="●"),
+    Group("2", layout="monadtall", label="●"),
+    Group("3", layout="monadtall", label="●"),
+    Group("4", layout="tile", label="●"),
+    Group("5", layout="max", label="●"),
+    Group("6", layout="floating", label="●"),
 ]
 
 for i in groups:
@@ -173,46 +227,67 @@ for i in groups:
     )
 
 ## ScratchPads
-layout_mus = { 'x': 0.30, 'y': 0.1, 'width': 0.40, 'height': 0.45, 'on_focus_lost_hide': False }
-layout_term = { 'x': 0.25, 'y': 0.1, 'width': 0.50, 'height': 0.45,'on_focus_lost_hide': False }
+layout_mus = {
+    "x": 0.30,
+    "y": 0.1,
+    "width": 0.40,
+    "height": 0.45,
+    "on_focus_lost_hide": False,
+}
+layout_term = {
+    "x": 0.25,
+    "y": 0.1,
+    "width": 0.50,
+    "height": 0.45,
+    "on_focus_lost_hide": False,
+}
 
-groups.append(ScratchPad("7", [
-    DropDown("terminal", terminal, **layout_term),
-    DropDown("music", "kitty -e musikcube", **layout_mus),
-]))
+groups.append(
+    ScratchPad(
+        "7",
+        [
+            DropDown("terminal", terminal, **layout_term),
+            DropDown("music", "kitty -e musikcube", **layout_mus),
+        ],
+    )
+)
 
-keys.extend([
-    Key([mod], 'F5', lazy.group["7"].dropdown_toggle("terminal")),
-    Key([mod], 'F6', lazy.group["7"].dropdown_toggle("music")),
-])
+keys.extend(
+    [
+        Key([mod], "F5", lazy.group["7"].dropdown_toggle("terminal")),
+        Key([mod], "F6", lazy.group["7"].dropdown_toggle("music")),
+    ]
+)
 
 # colorscheme
 colors = colors.Dracula
 
 layout_theme = {
-        "border_width": 2,
-        "margin": 13,
-        "border_focus": colors[4],
-        "border_normal": colors[3],
-} 
+    "border_width": 2,
+    "margin": 13,
+    "border_focus": colors[4],
+    "border_normal": colors[3],
+}
 layouts = [
-        layout.MonadTall(**layout_theme),
-        layout.Max(),
-        layout.Tile(
-            border_width=0,
-            margin=0,
-            border_focus=colors[4],
-            border_normal=colors[3],
-            ratio=0.5,
-),
+    layout.MonadTall(**layout_theme),
+    layout.Max(),
+    layout.Tile(
+        border_width=0,
+        margin=0,
+        border_focus=colors[4],
+        border_normal=colors[3],
+        ratio=0.5,
+    ),
 ]
 
 decoration_group = {
-        "foreground": colors[11],
-        "padding": 10,
-        "decorations": [
-            RectDecoration(colour=colors[1], radius=13, filled=True, padding_y=4, group=True)
-            ]
+    "foreground": colors[11],
+    "padding": 10,
+    "decorations": [
+        RectDecoration(
+            colour=colors[1], radius=13, filled=True, padding_y=4, group=True
+        )
+    ],
 }
 widget_defaults = dict(
     font="JetBrainsMono Nerd Font Bold",
@@ -227,68 +302,30 @@ screens = [
                 #########################
                 # Widget Configurations #
                 #########################
-                  widget.Spacer(length=5),
-                  widget.GroupBox(
+                widget.Spacer(length=5),
+                widget.GroupBox(
                     font="JetBrainsMono Nerd Font Bold",
                     active=colors[4],
                     inactive=colors[3],
                     disable_drag=True,
-                    highlight_method='block',
+                    highlight_method="block",
                     highlight_color=colors[12],
-                    this_current_screen_border = "#00000000",
+                    this_current_screen_border="#00000000",
                     block_highlight_text_color=colors[8],
-                    **decoration_group
-                ),
-                  widget.Spacer(length=10),
-                  widget.CurrentLayout(
-                      scale=0.5,
-                      **decoration_group,
-                ),    
-                  widget.Spacer(length=10),
-                  widget.TextBox(
-                      text="Apps",
-                      mouse_callbacks={"Button1": lambda: qtile.cmd_spawn(home + "/.config/rofi/scripts/rofi-drawer")},
-                      **decoration_group
+                    **decoration_group,
                 ),
                 widget.Spacer(length=bar.STRETCH),
-                widget.Clock(
-                    format="%b %d %I:%M %p",
-                    **decoration_group
-                ),
+                widget.Clock(format="%b %d %I:%M %p", **decoration_group),
                 widget.Spacer(length=bar.STRETCH),
                 widget.Bluetooth(
-                default_text='{connected_devices}',
-                fmt='󰂯 {}',
-                max_chars=10,
-                **decoration_group
+                    default_text="{connected_devices}",
+                    fmt="󰂯 {}",
+                    max_chars=10,
+                    **decoration_group,
                 ),
                 widget.Spacer(length=10),
-                widget.CPU(
-                    format='{load_percent}%',
-                    fmt=' {}',
-                    **decoration_group
-                ),
-                widget.HDD(
-                    format='{HDDPercent}%',
-                    fmt=' {}',
-                    **decoration_group
-                ),
-                widget.Memory(
-                    measure_mem='G',
-                    format='{MemUsed:.0f}{mm}/{MemTotal:.0f}{mm}',
-                    fmt='  {}',
-                    **decoration_group
-                ),
-                widget.Spacer(length=10),
-                widget.Volume(
-                    fmt="󰕾 {}",
-                    volume_app='wpctl',
-                    **decoration_group
-                ),
-                widget.Systray(
-                    icon_size=20,
-                    fmt="{}",
-                ),
+                widget.Volume(fmt="󰕾 {}", volume_app="wpctl", **decoration_group),
+                widget.Systray(icon_size=20, fmt="{}", **decoration_group),
                 widget.Spacer(length=5),
             ],
             ######################
@@ -297,15 +334,22 @@ screens = [
             38,
             margin=[10, 8, 0, 8],
             border_width=0,
-            background=colors[12]
+            background=colors[12],
         ),
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -316,8 +360,8 @@ bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
-        border_focus=colors[4],
-        border_normal=colors[3],
+    border_focus=colors[4],
+    border_normal=colors[3],
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
@@ -327,7 +371,7 @@ floating_layout = layout.Floating(
         Match(wm_class="ssh-askpass"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
-    ]
+    ],
 )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
@@ -344,10 +388,12 @@ wl_input_rules = None
 wl_xcursor_theme = None
 wl_xcursor_size = 24
 
+
 @hook.subscribe.startup_once
 def autostart():
-    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
     subprocess.Popen([home])
+
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
