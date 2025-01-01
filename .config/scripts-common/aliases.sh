cbd758528d752e9ce0984a968dc0815c03c89177
RVSmooth <riveeks.smooth@gmail.com>
2025-01-01 22:16:44 +0530

    chore(scripts-common): Add waypick alias in aliases

--- .config/scripts-common/aliases.sh
+++ .config/scripts-common/aliases.sh
@@ -44,3 +44,5 @@ alias yta-flac="yt-dlp --extract-audio --audio-format flac "
 alias yta-mp3="yt-dlp --extract-audio --audio-format mp3 "
 alias ytv-best="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 "
 
+#wayland
+alias waypick='grim -g "$(slurp -p)" -t ppm - | magick - -format "%[pixel:p{0,0}]" txt:- | tail -n 1 | cut -d " " -f 4 | wl-copy'
