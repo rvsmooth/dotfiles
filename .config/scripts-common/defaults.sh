6677e0fdabd5d4000f7b4d19b060c640d9445d5b
RVSmooth <riveeks.smooth@gmail.com>
2025-01-24 07:10:48 +0530

    chore(updates): Update defaults, rofi themes, rofi PowerMenu

--- .config/scripts-common/defaults.sh
+++ .config/scripts-common/defaults.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Function to display usage
-browser1="flatpak run one.ablaze.floorp"
+browser1="flatpak run org.mozilla.firefox"
 browser2="flatpak run com.brave.Browser"
-email="flatpak run eu.betterbird.Betterbird"
+email="flatpak run org.mozilla.Thunderbird"
 
 usage() {
   echo 'Usage: $0 [-b1|--browser1] for "$browser1"  or [-b2|--browser2] for "$browser2" or [-e|--email] for "$email"'
