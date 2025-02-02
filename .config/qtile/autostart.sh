45d9e05ce21a30089ed785fe29065d89d6e5e99c
RVSmooth <riveeks.smooth@gmail.com>
2025-02-02 08:33:16 +0530

    chore: Replace lxsession with polkit-gnome

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,6 +2,6 @@
 picom --daemon &
 dunst &
 feh --bg-fill $HOME/Pictures/wallpapers/default &
-/usr/bin/lxpolkit &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/emacs --daemon &
 # variety &
