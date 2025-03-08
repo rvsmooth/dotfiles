d46a4765de3e17d496a0e5a91c468ebdf778cee1
RVSmooth <riveeks.smooth@gmail.com>
2025-03-08 16:50:01 +0530

    style: qtile: colors: Update tokyonight colorscheme's accent color

--- .config/qtile/colors.py
+++ .config/qtile/colors.py
@@ -34,7 +34,7 @@ Tokyonight = [
     ["#16161E", "#16161E"],  # c1-background
     ["#C0CAF5", "#C0CAF5"],  # c2-foreground
     ["#565F89", "#565F89"],  # c3-grey
-    ["#9D7CD8", "#9D7CD8"],  # c4-accent
+    ["#82AAFF", "#82AAFF"],  # c4-accent
     ["#F7768E", "#F7768E"],  # c5-red
     ["#9ECE6A", "#9ECE6A"],  # c6-green
     ["#E0AF68", "#E0AF68"],  # c7-yellow
