bcda3ef076d5c57f4c820a94c35a1afcd64c1985
rvsmooth <riveeks.smooth@gmail.com>
2025-05-19 18:22:02 +0530

    [WIP] feat: Rofi: scripts: Improve Themer script

---
+++ .config/rofi/scripts/reload.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#______ _   _ _____                       _   _
+#| ___ \ | | /  ___|                     | | | |
+#| |_/ / | | \ `--. _ __ ___   ___   ___ | |_| |__
+#|    /| | | |`--. \ '_ ` _ \ / _ \ / _ \| __| '_ \
+#| |\ \\ \_/ /\__/ / | | | | | (_) | (_) | |_| | | |
+#\_| \_|\___/\____/|_| |_| |_|\___/ \___/ \__|_| |_|
+#
+# Copyright (c) 2024 rvsmooth
+# https://github.com/rvsmooth
+
+APP='Reloader'
+
+NOTIFY() {
+
+  notify-send --icon=view-refresh-symbolic "$APP" "$@"
+
+}
+set -x
+function reload_app() {
+  APP_PID=$(pgrep "$1")
+  APP_PATH=$(which "$1")
+  APP_NAME="$1"
+  if [ -n "$APP_PID" ]; then
+    kill -9 "$APP_PID"
+    NOTIFY Restarting "$APP_NAME"
+    "$APP_PATH"
+  else
+    "$APP_PATH"
+  fi
+}
+function kill_app() {
+  APP_PID=$(pgrep "$1")
+  if [ -n "$APP_PID" ]; then
+    kill -9 "$APP_PID"
+  else
+    echo "App is not running"
+  fi
+}
